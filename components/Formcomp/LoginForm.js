69cc68bb4eb42b35b5bc4936f28c9dfc857a8992
djbenjamin2001 <benjaminbatbayli@gmail.com>
2023-06-15 10:54:08 +0200

    login side og funktionalitet er fuldændt og klar til merging

--- components/Formcomp/LoginForm.js
+++ components/Formcomp/LoginForm.js
@@ -1,58 +1,75 @@
-"use client"
+"use client";
 
-
-import MeglerId from '@/app/meglere/[meglereId]/page';
-import {Formik,Field, Form, ErrorMessage} from 'formik';
-import { LoginSchema } from './formSchema';
-import { setCookie } from 'cookies-next';
-  
+import MeglerId from "@/app/meglere/[meglereId]/page";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { LoginSchema } from "./formSchema";
+import { setCookie } from "cookies-next";
 
 const LoginForm = () => {
- 
- 
+  return (
+    <div className="flex flex-col">
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={LoginSchema}
+        onSubmit={(values, { resetForm }) => {
+          fetch("https://dinmaegler.onrender.com/auth/local", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              identifier: values.email,
+              password: values.password,
+            }),
+          })
+            .then((response) => {
+              console.log(response);
+              if (response.status === 400) {
+                console.log("wrong user eller pword")
+              } else {
+                return response.json();
+              }
+            })
+            .then((data) => {
+              setCookie("Dogus", data.jwt);
+              window.location.replace("/");
+            });
 
-return ( <div className='flex flex-col'>
-        
-        <Formik  initialValues={{
-            email:"",
-            password:"",
-        }} validationSchema={LoginSchema}
-         onSubmit={(values, {resetForm}) => {
-            fetch("https://dinmaegler.onrender.com/auth/local", {
-                "method": "POST",
-                "headers": {
-                  "Content-Type": "application/json"
-                },
-                "body": JSON.stringify({
-                  "identifier": values.email,
-                  "password": values.password
-                })
-              }) .then(response => response.json())
-              .then(data => console.log(data))
-              
-            console.log(values.email)
-            resetForm()
-            setCookie("jwt", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NThhNWMyZjZiNWUwMDA0YWQ0ZGQ2MSIsImlhdCI6MTY4NjYzOTUwNCwiZXhwIjoxNjg5MjMxNTA0fQ.UTvd7TuMKHSXJ3YH057ICBF2RXEZprim94M_qyUVkj0");
-        }} >
+          console.log(values.email);
 
-           
-                  <Form method="post" className='space-y-5 ' >
-                    
-                <div className="flex-col  space-x-5 ">
-                    
-                    <Field className="border border-black h-[3rem]" placeholder="skriv din email her" type="email" name="email" id="email" />
-                    <ErrorMessage name="email"/>
-                </div>
-                <div>
-                <Field className=" border border-black h-[3rem] w-[29rem]" placeholder="skriv din email her" type="Password" name="password" id="emne" />
-                    <ErrorMessage name="password"/></div>
-                   
-                <button type='submit'>Send</button>
-            </Form>   
-    
-       
-        </Formik>
-    </div> );
-}
- 
+          resetForm();
+        }}
+      >
+        <Form action="/" method="post" className="space-y-10 p-5 text-center">
+          <div className="flex-col  space-x-5 ">
+            <Field
+              className="border border-black h-[3rem] w-[29rem]"
+              placeholder="skriv din email her"
+              type="email"
+              name="email"
+              id="email"
+            />
+            <ErrorMessage name="email" />
+          </div>
+          <div>
+            <Field
+              className=" border border-black h-[3rem] w-[29rem]"
+              placeholder="skriv din password her"
+              type="Password"
+              name="password"
+              id="password"
+            />
+            <ErrorMessage name="password" />
+          </div>
+
+          <button className="bg-blue text-white  h-[3rem] w-[29rem]" type="submit">Login</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
 export default LoginForm;
--- components/Header.js
+++ components/Header.js
@@ -7,27 +7,38 @@ import {BsHouses} from 'react-icons/bs'
 import Link from 'next/link'
 import Navigation from './Navigation'
 import { getCookie, setCookie } from 'cookies-next';
-export const getServerSideProps = async ({ req, res }) => {
+import { cookies } from 'next/dist/client/components/headers'
+import Image from 'next/image'
+/* export const getServerSideProps = async ({ req, res }) => {
     setCookie("jwt", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NThhNWMyZjZiNWUwMDA0YWQ0ZGQ2MSIsImlhdCI6MTY4NjYzOTUwNCwiZXhwIjoxNjg5MjMxNTA0fQ.UTvd7TuMKHSXJ3YH057ICBF2RXEZprim94M_qyUVkj0")
  const cookie =  getCookie('jwt', { req, res });
   
 
     return { props: {cookie} };
-  };
-const Header = ({cookie}) => {
-  console.log(cookie)
+    
+  }; */
+  
+
+const Header = () => {
+const cookieStore = cookies()
+const theme = cookieStore.get('Dogus')
+
+console.log(theme)
     return ( <>
         <section className="flex  bg-blue  p-[1rem] justify-between text-white	">
             <div className='flex space-x-4'>
 <FaPaperPlane /><p>4000@dinmaegler.com</p>
 <BsTelephoneFill /><p>+45 7070 4000</p></div>
 <div className='flex justify-items-end '>
-<FaUser /><Link  href={"loginside"}>Log ind</Link>
+ 
+<FaUser />{theme ? <Link href={"logouthelper"}> <p>{theme.name}</p>  log out</Link> : <Link  href={"loginside"}>Log ind</Link>  }
  </div>
         </section>
-      <header className='flex p-[1rem] bg-white'>
-<BsHouses className='text-[4rem] text-blue'/><h1 className='text-blue font-bold mt-[1.5rem] text-[2rem]'>DIN MÆGLER</h1>
+      <header className='flex p-[1.5rem] px-[5rem] bg-white'>
+    
+    <Link href={"/"}>  <Image className='' src="/maegler.png" width={296} height={89} alt='sitelogo'></Image></Link>
 <Navigation />
+
       </header>
       </>
      );
